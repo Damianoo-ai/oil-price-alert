7216a9e62b665c9f0b84064df1c424b375a0c981
Damianoo-ai <damiano.obetti@gmail.com>
2026-04-15 21:57:42 +0200

    handling of some exceptions raise by the HTTP call done (especially yfinance)

--- main.py
+++ main.py
@@ -2,27 +2,47 @@ from news_sender import NewsSender
 import yfinance as yf
 from twilio.rest import Client
 import os
-import requests
 from dotenv import load_dotenv
+from url_manager import shorten_url
+import time
+
 
 load_dotenv()  
 
 # Change the value if needed
 SELECTED_TICKER = {
-    'BZ=F': 'Brent Oil'
+    'BZ=F': 'Brent Crude'
 }
 
 ns = NewsSender()
 ticker = list(SELECTED_TICKER.keys())[0]
-brent = yf.Ticker(ticker)
-df = brent.history(period="5d")
+
+df = None
+for attempt in range(3):
+    try:
+        brent = yf.Ticker(ticker)
+        df = brent.history(period="5d")
+    except Exception as e:
+        print(f'Yahoo finance ERROR:\n {e}')
+        df = None
+    
+    if df is not None or not df.empty:
+        break
+    
+    print(f"Retry {attempt + 1}/3")
+    time.sleep(2)
+    
+if df.empty or df is None:
+    print("Empty dataframe")
+else:
+    if df['Close'].isna().any():
+        print('NaN values present')
 
 print(df)
-
 # Find the increase/decrease of the price
 positive_delta = False
-today_price = df["Close"].iloc[-1]
-yest_price = df["Close"].iloc[-2]
+today_price = df["Low"].iloc[-1]
+yest_price = df["Low"].iloc[-2]
 delta_price = today_price - yest_price
 if delta_price > 0:
     positive_delta = True
@@ -36,37 +56,23 @@ print(f'Price change: {symbol}{round(percent_price, 2)}%')
 
 # Create the news, filter and save in a file
 data = ns.create_news()
-filtered = ns.filter_news(data)
-saved = ns.save_news(filtered)
-print(f'Atleast one news? {saved}Total News:{ns.count_news}')
+filter_news = ns.filter_news(data)
+saved = ns.save_news(filter_news)
+print(f'Atleast one news? {saved}\nTotal News:{ns.count_news}')
 
 
 if abs(percent_price) >= 5:
     client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
 
-    for art in filtered:
-        bitly_header = {
-            "Authorization": f"Bearer {os.getenv('BITLY_TOKEN')}",
-            "Content-Type": "application/json"
-        }
-        bitly_par = {
-            'long_url': art['url'],
-        }
+    for art in filter_news:
 
-        short_url = requests.post(
-            url='https://api-ssl.bitly.com/v4/shorten',
-            headers=bitly_header,
-            json=bitly_par
-        )
+        s_link = shorten_url(link=art['url'])
         
-        if short_url.status_code not in (201,200):
-            short_url = None
-
         message = client.messages.create(
             body=(
                 f"{SELECTED_TICKER[ticker]}: {symbol}{round(percent_price, 2)}%\n"
                 f"{art['title']}\n"
-                f"{short_url.json()['link'] if short_url else ''}"
+                f"{s_link.json()['link'] if s_link else ''}"
             ),
             from_=os.getenv('TWILIO_TRIAL_NUMBER'),
             to=os.getenv('TEST_NUMBER')
---
+++ url_manager.py
@@ -0,0 +1,56 @@
+import requests
+import os
+
+
+def shorten_url(link):
+    
+    if link is None:
+        print('BITLY: Url not found')
+        return None
+    
+    bitly_header = {
+        "Authorization": f"Bearer {os.getenv('BITLY_TOKEN')}",
+        "Content-Type": "application/json"
+    }
+    
+    payload = {
+        'long_url': link,
+    }
+
+    try:
+        response = requests.post(
+            url='https://api-ssl.bitly.com/v4/shorten',
+            headers=bitly_header,
+            json=payload,
+            timeout=5
+        )
+    except requests.exceptions.Timeout as e:
+        print(f'BITLY Timeout overcomed')
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+        return None
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return None
+    except Exception as e:
+        print(e)
+        return None
+    
+        
+    if response.status_code not in (201,200):
+        print(f'BITLY HTTP Error: {response.status_code}{response.text}')
+        return None
+    
+    try:
+        data:dict = response.json()
+    except ValueError as e:
+        print(f'Invalid JSON {e}')
+        return None
+    
+    link = data.get('link')
+
+    if not link:
+        print('BITLY: link not present in the response')
+        return None
+    
+    return link
